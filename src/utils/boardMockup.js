729ad5415a0b04bae60580fec3da94d2a81ae026
Sarin Udompanit <sarin_udo@sendit.asia>
2018-06-22 22:27:11 +0700

    Add boardmockup data

---
+++ src/utils/boardMockup.js
@@ -0,0 +1,23 @@
+export const boards = [
+  {
+    id: 'board-1',
+    title: 'board-1',
+    cards: [
+      { id: 'card-1', title: 'card-1', description: 'card-1 description' },
+    ],
+  },
+  {
+    id: 'board-2',
+    title: 'board-2',
+    cards: [
+      { id: 'card-2', title: 'card-2', description: 'card-2 description' },
+    ],
+  },
+  {
+    id: 'board-3',
+    title: 'board-3',
+    cards: [
+      { id: 'card-3', title: 'card-3', description: 'card-3 description' },
+    ],
+  },
+]
