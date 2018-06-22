d2773eeaf0a6ff82e7f6f23567589e0dc4575430
Sarin Udompanit <sarin_udo@sendit.asia>
2018-06-22 23:28:50 +0700

    Styled Card and Board

--- src/components/Boards/Boards.board.js
+++ src/components/Boards/Boards.board.js
@@ -1,13 +1,38 @@
 import React from 'react'
 import Card from '../../components/Card'
+import styled from 'styled-components'
+
+const BoardContainer = styled.div`
+  width: 272px;
+  margin: 0 4px;
+  height: 100%;
+  box-sizing: border-box;
+  display: inline-block;
+  vertical-align: top;
+  white-space: nowrap;
+`
+
+const StyledBoard = styled.div`
+  padding: 12px;
+  background: #e2e4e6;
+  border-radius: 3px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  max-height: 100%;
+  position: relative;
+  white-space: normal;
+`
 
 const Board = ({ board }) => {
   const cardItems = board.cards.map(c => <Card key={c.id} card={c} />)
   return (
-    <div>
-      <p>{board.title}</p>
-      {cardItems}
-    </div>
+    <BoardContainer>
+      <StyledBoard>
+        <p>{board.title}</p>
+        {cardItems}
+      </StyledBoard>
+    </BoardContainer>
   )
 }
 
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+  background-color: #fff;
+  border-radius: 3px;
+  box-shadow: 0 1px 0 #ccc;
+  cursor: pointer;
+  display: block;
+  margin-bottom: 8px;
+  max-width: 300px;
+  min-height: 20px;
+  position: relative;
+  text-decoration: none;
+  z-index: 0;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`
+
+const Title = styled.p`
+  padding: 0 10px;
+`
 
 const Card = ({ card }) => {
   return (
-    <div>
-      <p>{card.title}</p>
-    </div>
+    <StyledCard>
+      <Title>{card.title}</Title>
+    </StyledCard>
   )
 }
 
