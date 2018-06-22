a5b9c3b7eec5dc244cda73a30d9d21908d4deeaa
Sarin Udompanit <sarin_udo@sendit.asia>
2018-06-22 22:19:59 +0700

    Add HeaderBar in HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import HeaderBar from '../../components/HeaderBar'
+import Logo from '../../images/logo.png'
+
+const HomePageContainer = styled.div``
+
+const StyledHeader = styled(HeaderBar)`
+  && {
+    background: white;
+  }
+`
+
+const StyledLogo = styled.img`
+  display: block;
+  margin: 0 auto;
+  width: 100px;
+`
 
 class HomePage extends Component {
   render() {
     return (
-      <div>
-        <HeaderBar>Trello</HeaderBar>
-      </div>
+      <HomePageContainer>
+        <StyledHeader>
+          <StyledLogo src={Logo} />
+        </StyledHeader>
+      </HomePageContainer>
     )
   }
 }
